13271dab829257e4d568aec92d100e0fd3f7020c
Lola Applegate <laurenemilya@gmail.com>
2022-05-22 14:45:24 -0700

    added some front end routes

---
+++ controllers/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const userRoutes = require("./userRoutes");
+router.use("/api/users",userRoutes);
+
+const blogRoutes = require("./blogRoutes");
+router.use("/api/blogs",blogRoutes);
+
+const commentRoutes = require("./commentRoutes");
+router.use("/api/comments",commentRoutes);
+
+const frontEnd = require("./frontEndRoutes");
+router.use("/",frontEnd);
+
+router.get("/showsessions",(req,res)=>{
+    res.json(req.session)
+})
+
+module.exports = router;
