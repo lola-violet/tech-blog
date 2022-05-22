f69943b195228cd3e3d1a28d9753fa56f91352c0
Lola Applegate <laurenemilya@gmail.com>
2022-05-22 12:27:41 -0700

    created all CRUD user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,39 +6,69 @@ const User = require('../models/User');
 router.get("/", (req, res) => {
     User.findAll()
     .then(dbUsers => {
-        res.json(dbUsers)
+        res.json(dbUsers);
     })
     .catch(err => {
         console.log(err);
         res.status(500).json({ msg: "500 Internal Server Error", err });
     })
-})
+});
 
 // Find one user by ID
 router.get("/:id", (req, res) => {
     User.findByPk(req.params.id)
     .then(dbUser => {
-        res.json(dbUser)
+        res.json(dbUser);
     })
     .catch(err => {
         console.log(err);
         res.status(500).json({ msg: "500 Internal Server Error", err });
     })
-})
+});
 
 // Create new user
 router.post("/", (req, res) => {
     User.create(req.body)
     .then(newUser => {
-        res.json(newUser)
+        res.json(newUser);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ msg: "500 Internal Server Error", err });
+    })
+});
+
+// Update existing user by ID
+router.put("/:id", (req, res) => {
+    User.update(req.body, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(updatedUser => {
+        res.json(updatedUser);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ msg: "500 Internal Server Error", err });
+    })
+});
+
+// Delete user by ID
+router.delete("/:id", (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(deletedUser => {
+        res.json(deletedUser);
     })
     .catch(err => {
         console.log(err);
         res.status(500).json({ msg: "500 Internal Server Error", err });
     })
 })
-
-
 
 
 module.exports = router;
